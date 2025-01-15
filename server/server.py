718fed5dd3baa086072c92a4f48cdd3d690904bb
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-15 13:44:41 -0500

    changed to send message from backend to front end "Hello World"

--- server/server.py
+++ server/server.py
@@ -3,9 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World'
+@app.route('/api/home', methods=['GET'])
+def return_home():
+    return jsonify({
+        'message':'Hello World'
+    })
+
 
 if __name__ == '__main__':
     app.run(debug=True)
