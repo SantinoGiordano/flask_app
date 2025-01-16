46ce841f2821782a0552053f3039e1cbc2220710
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-16 11:30:58 -0500

    added people useState, and set a data.people

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,17 +3,19 @@ import { useEffect, useState } from "react";
 
 function Index() {
   const [message, setMessage] = useState("Loading");
+  const [people, setPeople] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        console.log("Fetching data...");
         const response = await fetch("http://localhost:8080/api/home");
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
         setMessage(data.message);
+        setPeople(data.people);
+        console.log(data.people)
       } catch (error) {
         console.error("Error fetching data:", error);
       }
