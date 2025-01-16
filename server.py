c8206e1b7372c4f8d9fcf999286cb904b59c66b2
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-16 17:18:37 -0500

    added more imports and what will be used

--- server.py
+++ server.py
@@ -2,17 +2,26 @@
 # to run client, cd .. , then cd client
 # while in flask_app run .\env\Scripts\Activate.ps1 to activate enviorment
 #cors has issue with 5000
+
 from random import randint
 from flask import Flask, jsonify 
 from flask_cors import CORS
+from sqlalchemy import create_engine, ForeignKey , Column, String, Integer, CHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+
+
 app = Flask(__name__)
 CORS(app)
+
 # Run: python server.py , and go to localhost:(number)'/api/home' == num run dev
-@app.route('/api/home', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def return_home():
     return jsonify({
         'message':'Hello World from the backend',
-        'people': ['Jack','Sam','Tom','Jerffery','Aldi'],
+        'people': ['Jack','Sam','Tom','Jeffery','Aldi'],
         'number':randint(1,30)
     })
 
