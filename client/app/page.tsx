c700676d81a0923bebd3e3b1fb27f4a396fa18bc
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-16 10:52:08 -0500

    create a useEffect hook

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,24 @@
 import { useEffect, useState } from "react"
 
 const Index = () => {
+  
+  useEffect(() => {
+    // Define the async function inside the useEffect
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost/api/home');
+        const data = await response.json();
+        console.log(data); 
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+      console.log("hello"); 
+    };
+  
+    fetchData(); 
+  }, []);
+
+
   return (
     <div>Hello world </div>
   )
