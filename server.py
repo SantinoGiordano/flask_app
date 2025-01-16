4a605625611d902160fea096e6e365bab32be514
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-16 12:17:58 -0500

    added a random number to data to play with variables

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 # to run client, cd .. , then cd client
 # while in flask_app run .\env\Scripts\Activate.ps1 to activate enviorment
 #cors has issue with 5000
+from random import randint
 from flask import Flask, jsonify 
 from flask_cors import CORS
 app = Flask(__name__)
@@ -11,9 +12,9 @@ CORS(app)
 def return_home():
     return jsonify({
         'message':'Hello World from the backend',
-        'people': ['Jack','Sam','Tom','Jerffery','Aldi']
+        'people': ['Jack','Sam','Tom','Jerffery','Aldi'],
+        'number':randint(1,30)
     })
-
 
 if __name__ == '__main__':
     app.run(debug=True,port=8080)
